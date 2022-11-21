0affbbd0bcaf45939a14ad13ca5844b8d34ce96f
Roma Ivanov <rivanov@inno.tech>
2022-11-21 11:44:25 +0300

    init

---
+++ src/test/java/ru/research/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.research.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
